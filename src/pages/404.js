15e3d042e6a25140d0ec6b8b247d6bff2663e9fb
Cletus Dias <cletus6@gmail.com>
2020-10-12 22:13:22 -0400

    set up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,21 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
-import SEO from "../components/SEO"
+import React from 'react';
+import Layout from '../components/Layout';
+import { Link } from 'gatsby';
+import SEO from '../components/SEO';
 
 const Error = () => {
-  return <h2>error page</h2>
-}
+	return (
+		<Layout>
+			<main className='error-page'>
+				<div className='error-container'>
+					<h1>Nothing to see here...</h1>
+					<Link to='/' className='btn'>
+						Back Home
+					</Link>
+				</div>
+			</main>
+		</Layout>
+	);
+};
 
-export default Error
+export default Error;
