85ba7fdd3aa905069f5d2f7210350c9c942b5a2f
Cletus Dias <cletus6@gmail.com>
2020-10-21 21:41:10 -0400

    gitignore working properly

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-gtag/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"G-L4BJP7JHRR","head":true},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
